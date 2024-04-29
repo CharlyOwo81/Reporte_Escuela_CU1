233e4a63aad5c6e7cea9709bbe2d28f20983417a
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-04-28 21:04:55 -0700

    Lógica de IncidenciasBO en teoría terminada

--- ReportesNegocios/src/main/java/negocios/bo/IncidenciasBO.java
+++ ReportesNegocios/src/main/java/negocios/bo/IncidenciasBO.java
@@ -4,13 +4,15 @@
  */
 package negocios.bo;
 
-import java.util.GregorianCalendar;
-import java.util.LinkedList;
 import java.util.List;
 import dto.AlumnoDTO;
 import dto.DocenteDTO;
 import dto.ReporteDTO;
-import persistencia.entidades.NivelIncidenciaPersistencia;
+import java.util.ArrayList;
+import org.bson.types.ObjectId;
+import persistencia.entidades.ReporteEntity;
+import persistencia.persistencia.IReportesDAO;
+import persistencia.persistencia.ReportesDAO;
 
 /**
  *
@@ -18,95 +20,112 @@ import persistencia.entidades.NivelIncidenciaPersistencia;
  */
 public class IncidenciasBO implements IIncidenciasBO{
 
-    List<ReporteDTO> reportes ;
+    private IReportesDAO reportesDAO;
     
     public IncidenciasBO() {
-        cargarDatos() ;
+        this.reportesDAO  = new ReportesDAO();
     }
     
     @Override
-    public ReporteDTO validarReporte(ReporteDTO reporteNuevo) {
-        for (int i = 0; i < reportes.size(); i++) {
-            if(reportes.get(i).getAlumno().getCURP().equalsIgnoreCase(reporteNuevo.getAlumno().getCURP())) {
-                reportes.set(i, reporteNuevo) ;
-                return reportes.get(i) ;
-            }
-        }
-        return null ;
+    public ReporteDTO validarReporte(ReporteDTO reporteDto) {
+        ReporteEntity reporteObtenido = new ReporteEntity();
+        
+        reporteObtenido.setId(new ObjectId(reporteDto.getId()));
+        reporteObtenido.setValidado(reporteDto.isValidado());
+        
+        reporteObtenido = reportesDAO.validarReporte(reporteObtenido);
+        
+        if(reporteObtenido == null) return null;
+        AlumnoDTO alumnoObtenido = new AlumnoDTO(
+                reporteObtenido.getAlumno().getId().toString(),
+                reporteObtenido.getAlumno().getCURP(),
+                reporteObtenido.getAlumno().getNombre(),
+                reporteObtenido.getAlumno().getApellidoP(),
+                reporteObtenido.getAlumno().getApellidoM(),
+                reporteObtenido.getAlumno().getGradoGrupo(),
+                reporteObtenido.getAlumno().getUrlFoto(),
+                reporteObtenido.getAlumno().getTelefonoTutor()
+        );
+
+        DocenteDTO docenteObtenido = new DocenteDTO(
+                reporteObtenido.getDocente().getId().toString(),
+                reporteObtenido.getDocente().getCURP(),
+                reporteObtenido.getDocente().getNombre(),
+                reporteObtenido.getDocente().getApellidoP(),
+                reporteObtenido.getDocente().getApellidoM()
+        );
+
+        return new ReporteDTO(
+                reporteObtenido.getId().toString(),
+                alumnoObtenido,
+                docenteObtenido,
+                reporteObtenido.getNivelIncidencia(),
+                reporteObtenido.getDescripcion(),
+                reporteObtenido.getMotivo(),
+                reporteObtenido.getFechaHora(),
+                reporteObtenido.isNotificado(),
+                reporteObtenido.isValidado()
+        );
+        
     }
 
     @Override
-    public boolean notificarReporte(ReporteDTO reporteNuevo) {
-        for (int i = 0; i < reportes.size(); i++) {
-            if(reportes.get(i).getAlumno().getCURP().equalsIgnoreCase(reporteNuevo.getAlumno().getCURP())) {
-                reportes.set(i, reporteNuevo) ;
-                return true ;
-            }
-        }
+    public boolean notificarReporte(ReporteDTO reporteDto) {
+//        for (int i = 0; i < reportes.size(); i++) {
+//            if(reportes.get(i).getAlumno().getCURP().equalsIgnoreCase(reporteNuevo.getAlumno().getCURP())) {
+//                reportes.set(i, reporteNuevo) ;
+//                return true ;
+//            }
+//        }
         return false ;
     }
 
     @Override
     public List<ReporteDTO> recuperarReportes() {
-        return reportes ;
+        List<ReporteEntity> reportesEntity = reportesDAO.recuperarReportes();
+        if (reportesEntity == null && reportesEntity.isEmpty()) {
+            return null;
+        }
+        
+        List<ReporteDTO> reportesDto = new ArrayList<>();
+        
+        for (ReporteEntity reporteObtenido : reportesEntity) {
+
+            AlumnoDTO alumnoDto = new AlumnoDTO(
+                    reporteObtenido.getAlumno().getId().toString(),
+                    reporteObtenido.getAlumno().getCURP(),
+                    reporteObtenido.getAlumno().getNombre(),
+                    reporteObtenido.getAlumno().getApellidoP(),
+                    reporteObtenido.getAlumno().getApellidoM(),
+                    reporteObtenido.getAlumno().getGradoGrupo(),
+                    reporteObtenido.getAlumno().getUrlFoto(),
+                    reporteObtenido.getAlumno().getTelefonoTutor()
+            );
+
+            DocenteDTO docenteDto = new DocenteDTO(
+                    reporteObtenido.getDocente().getId().toString(),
+                    reporteObtenido.getDocente().getCURP(),
+                    reporteObtenido.getDocente().getNombre(),
+                    reporteObtenido.getDocente().getApellidoP(),
+                    reporteObtenido.getDocente().getApellidoM()
+            );
+
+            ReporteDTO reporteDTO = new ReporteDTO(
+                    reporteObtenido.getId().toString(),
+                    alumnoDto,
+                    docenteDto,
+                    reporteObtenido.getNivelIncidencia(),
+                    reporteObtenido.getDescripcion(),
+                    reporteObtenido.getMotivo(),
+                    reporteObtenido.getFechaHora(),
+                    reporteObtenido.isNotificado(),
+                    reporteObtenido.isValidado()
+            );
+            
+            reportesDto.add(reporteDTO);
+        }
+        return reportesDto;
     }
-    
-    private void cargarDatos() {
-        reportes = new LinkedList<>() ;
-        AlumnoDTO alumno1 = new AlumnoDTO ("IUVO040706HSLNLLA2", "Oliver", "Inzunza", "Valle", "3B", 6878767707L, "src/main/java/fotos/foto_oliver.jpeg") ;
-        AlumnoDTO alumno2 = new AlumnoDTO ("CAMG040802HSRSLLA5", "Gael Rafael", "Castro", "Molina", "2A", 6448750493L, "src/main/java/fotos/foto_gael.jpeg") ;
-        AlumnoDTO alumno3 = new AlumnoDTO ("AOMA040301HSRPNSA3", "Asiel", "Apodaca", "Monge", "1C", 6448094837L, "src/main/java/fotos/foto_asiel.jpeg") ;
-        AlumnoDTO alumno4 = new AlumnoDTO ("OUQA040309HSRLRMA5", "Amós Helí", "Olguín", "Quiróz", "3A", 6441840394L, "src/main/java/fotos/foto_amos.jpeg") ;
-        AlumnoDTO alumno5 = new AlumnoDTO ("AEPG040701HSRRRMA6", "Gamaliel", "Armenta", "Perez", "1C", 6878094837L, "src/main/java/fotos/foto_gama.jpg") ;
-        AlumnoDTO alumno6 = new AlumnoDTO ("RITJ010224HSRXPNA4", "Juan Pablo", "Del Río", "Tapia", "3C", 6878094837L, "src/main/java/fotos/foto_jp.jpeg") ;
-
-        DocenteDTO docente1 = new DocenteDTO("GALJ940519HDFLRN05", "Juan", "García", "López") ;
-        DocenteDTO docente2 = new DocenteDTO("ROHM000712MDFDRR07", "María", "Rodríguez", "Hernández") ;
-        DocenteDTO docente3 = new DocenteDTO("PEMC010224HDFRRL00", "Carlos", "Pérez", "Martínez") ;
-
-        ReporteDTO reporte1 = new ReporteDTO(alumno4, docente1,
-                NivelIncidenciaPersistencia.GRAVE, "Se encontró al alumno fumando un tabaco en la entrada de los baños de hombres",
-                "Fumar Tabaco dentro de la Escuela", new GregorianCalendar(), false, false) ;
-        ReporteDTO reporte2 = new ReporteDTO(alumno1, docente1,
-                NivelIncidenciaPersistencia.LEVE, "El alumno alzó la voz y ofendió con groserías a su compañero en el Aula",
-                "Hablar con Lenguaje Inapropiado", new GregorianCalendar(), false, false) ;
-        ReporteDTO reporte3 = new ReporteDTO(alumno3, docente2,
-                NivelIncidenciaPersistencia.SEVERO, "Este alumno inició una pelea y su compañero al que agredió no pudo defenderse, fué leve y no pasó a mayores",
-                "Iniciar Pelea", new GregorianCalendar(), false, false) ;
-        ReporteDTO reporte4 = new ReporteDTO(alumno5, docente2,
-                NivelIncidenciaPersistencia.LEVE, "El alumno lleva 1 semana entera faltando a mi clase de Matemáticas, no ha presentado justificante y diario se le puede ver como se va de otras clases",
-                "Inasistencia frecuente", new GregorianCalendar(), false, false) ;
-        ReporteDTO reporte5 = new ReporteDTO(alumno6, docente3,
-                NivelIncidenciaPersistencia.LEVE, "Este alumno estuvo tirando basura dentro de la institución sin importarle las constantes llamadas de atención que le hice",
-                "Contaminación ambiental en la institución", new GregorianCalendar(), false, false) ;
-        ReporteDTO reporte6 = new ReporteDTO(alumno2, docente3,
-                NivelIncidenciaPersistencia.GRAVE, "El alumno estuvo ingiriendo bebidas alcoholicas a media clase desde su termo",
-                "Consumo de bebidas alcoholicas dentro del salón", new GregorianCalendar(), false, false) ;
-
-        reportes.add(reporte1) ;
-        reportes.add(reporte2) ;
-        reportes.add(reporte3) ;
-        reportes.add(reporte4) ;
-        reportes.add(reporte5) ;
-        reportes.add(reporte6) ;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
