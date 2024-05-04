5b6eab771610ce359ca9a439e2980fc40347907c
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-05-04 12:33:28 -0700

    Se modificó conexión a base de datos

--- ReportesPersistencia/src/main/java/persistencia/conexionBD/ConexionMongo.java
+++ ReportesPersistencia/src/main/java/persistencia/conexionBD/ConexionMongo.java
@@ -3,19 +3,15 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
-import persistencia.entidades.ReporteEntity;
 
 /**
- * Clase que gestiona la conexión a la base de datos MongoDB y proporciona métodos para abrir y cerrar la conexión, así como para obtener la colección de películas.
+ * Clase que gestiona la conexión a la base de datos MongoDB y proporciona métodos para abrir y cerrar la conexión, así como para obtener la base de datos de Expediente.
  * La clase utiliza la biblioteca oficial de MongoDB para Java.
- * Requiere una instancia de MongoDB en ejecución en localhost:27017 y una base de datos llamada "Rentas" con una colección llamada "Peliculas".
  * 
  * @author Asiel Apodaca Monge
  */
@@ -23,19 +19,15 @@ public class ConexionMongo {
     // Cadena de conexión a la instancia de MongoDB
     private static final String cadenaConexion = "mongodb://localhost:27017";
     // Nombre de la base de datos
-    private static final String nombreBd = "Escuela";
-    // Nombre de la colección
-    private static final String nombreCol = "Reportes";
+    private static final String nombreBd = "Expediente";
     
     // Cliente de MongoDB
     private static MongoClient mongoClient;
     // Base de datos MongoDB
     private static MongoDatabase baseDeDatos;
-    // Colección MongoDB para películas
-    private static MongoCollection<ReporteEntity> coleccion;
     
     /**
-     * Abre una conexión a la base de datos MongoDB y obtiene la colección de películas.
+     * Abre una conexión a la base de datos MongoDB.
      */
     public static void abrirConexion() {
         
@@ -44,19 +36,18 @@ public class ConexionMongo {
         
         mongoClient = MongoClients.create(configuracion);
         baseDeDatos = mongoClient.getDatabase(nombreBd);
-        coleccion = baseDeDatos.getCollection(nombreCol, ReporteEntity.class);
     }
     
     /**
      * Obtiene la colección de películas. Si la conexión no está abierta, la abre antes de obtener la colección.
      * 
-     * @return La colección de películas.
+     * @return La base de datos de Expediente.
      */
-    public static MongoCollection<ReporteEntity> obtenerColeccion() {
-        if(coleccion == null) {
+    public static MongoDatabase obtenerBaseDeDatos() {
+        if(baseDeDatos == null) {
             abrirConexion();
         }
-        return coleccion;
+        return baseDeDatos;
     }
     
     /**
