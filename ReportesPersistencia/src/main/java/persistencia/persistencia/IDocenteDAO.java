a0d8115d7e90109db7570336a96ab8035d7cfff9
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-05-03 23:57:40 -0700

    Create IDocenteDAO.java

---
+++ ReportesPersistencia/src/main/java/persistencia/persistencia/IDocenteDAO.java
@@ -0,0 +1,12 @@
+package persistencia.persistencia;
+
+import persistencia.entidades.DocenteEntity;
+
+/**
+ *
+ * @author Asiel Apodaca Monge
+ */
+public interface IDocenteDAO {
+    public DocenteEntity obtenerDocente(DocenteEntity de);
+    
+}
